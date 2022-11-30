58bce9f8a7bd928bba1acff868c7893bc7ade8de
Dmitry-Basuk <119036651+Dmitry-Basuk@users.noreply.github.com>
2022-11-30 15:13:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿/* Напишите программу, которая на вход принемает число и выдает его квадрат(число умноженное на само себя).
+﻿/* Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
-Например:
-4->16
--3 -> 9
--7 -> 49
+a = 25; b = 5 -> yes
+a = 2; b = 10 -> no
+a = 9; b = -3 -> yes
+a = -3; b = 9 -> no
 */
+Console.WriteLine("Введите первое число:");
+string numberOneStr = Console.ReadLine();
 
+Console.WriteLine("Введите второе число:");
+string numberTwoStr = Console.ReadLine();
 
-Console.WriteLine("Введите число:");
-string numberStr = Console.ReadLine();
-int number = Convert.ToInt32(numberStr);
-int result = number * number;
+int numberOne = Convert.ToInt32(numberOneStr);
+int numberTwo = Convert.ToInt32(numberTwoStr);
 
-Console.WriteLine($"Квадрат числа {number} равен = {result}"); /// " Кввадрат числа 2 равен = 4"
+if (numberOne == numberTwo * numberTwo)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
