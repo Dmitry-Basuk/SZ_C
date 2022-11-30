b41b7e958e1e272d54cbbd2c286c651301e8c667
Basuk Dmitry <basukdmity@mail.ru>
2022-11-30 14:37:48 +0300

    решение задачи на квадратный корень

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принемает число и выдает его квадрат(число умноженное на само себя).
+
+Например:
+4->16
+-3 -> 9
+-7 -> 49
+*/
+
+
+Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен = {result}"); /// " Кввадрат числа 2 равен = 4"
